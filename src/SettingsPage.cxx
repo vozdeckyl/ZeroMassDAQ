473508ff73f3559a8fdba2d7725a459cdb0f6ef3
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-01 14:37:54 +0000

    added slider into the settings page

--- src/SettingsPage.cxx
+++ src/SettingsPage.cxx
@@ -1,18 +1,26 @@
 #include "SettingsPage.hpp"
 
-SettingsPage::SettingsPage()
+SettingsPage::SettingsPage() :
+    m_adjustment(Gtk::Adjustment::create(1000, 500, 10000, 10, 10, 0))
 {
     m_label.set_markup("<span font=\"25\">Settings</span>");
     m_label.set_margin_bottom(20);
 
+	m_scale.set_adjustment(m_adjustment);
+	m_scale.set_size_request(600,-1);
+	m_scale.set_digits(0);
+	
 	m_mainLayoutGrid.insert_column(0);
     m_mainLayoutGrid.insert_row(0);
     m_mainLayoutGrid.insert_row(0);
-
+	
     m_mainLayoutGrid.attach(m_label,0,0);
-    
+	m_mainLayoutGrid.attach(m_scale,0,1);
+	
+	
     add(m_mainLayoutGrid);
-
+	
 	m_label.show();
+	m_scale.show();
 	m_mainLayoutGrid.show();
 }
--- src/SettingsPage.hpp
+++ src/SettingsPage.hpp
@@ -6,10 +6,12 @@
 class SettingsPage : public Gtk::Box {
 public:
     SettingsPage();
-
+	
 private:
     Gtk::Label m_label;
     Gtk::Grid m_mainLayoutGrid;
+	Glib::RefPtr<Gtk::Adjustment> m_adjustment;
+	Gtk::Scale m_scale;
 };
 
 #endif
