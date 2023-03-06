47d0364f2fcd25d3c89fdc2d093477444c9fb4bf
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-06 18:05:15 +0000

    added selection for gauge dial channel

--- src/SettingsPage.cxx
+++ src/SettingsPage.cxx
@@ -20,10 +20,44 @@ SettingsPage::SettingsPage() :
     m_mainLayoutGrid.insert_row(0);
     m_mainLayoutGrid.insert_row(0);
 	m_mainLayoutGrid.insert_row(0);
+	m_mainLayoutGrid.insert_row(0);
+	m_mainLayoutGrid.insert_row(0);
 	
     m_mainLayoutGrid.attach(m_label,0,0);
 	m_mainLayoutGrid.attach(m_samplingIntervalScale,0,2);
 	m_mainLayoutGrid.attach(m_samplingIntervalLabel,0,1);
+	m_mainLayoutGrid.attach(m_dialChannelLabel,0,3);
+	m_mainLayoutGrid.attach(m_ScrolledWindow,0,4);
+
+	m_dialChannelLabel.set_markup("<span font=\"12\">Channel(s) to be displayed on the gauge dial</span>");
+	m_dialChannelLabel.set_halign(Gtk::Align::ALIGN_START);
+	m_dialChannelLabel.set_margin_top(30);
+	m_dialChannelLabel.set_margin_bottom(10);
+	
+	m_ScrolledWindow.add(m_dialChannelView);
+	m_ScrolledWindow.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+	m_ScrolledWindow.set_size_request(-1,200);
+	
+	m_refListStore = Gtk::ListStore::create(m_Columns);
+
+	Gtk::TreeModel::iterator iter = m_refListStore->append();
+	Gtk::TreeModel::Row row = *iter;
+	row[m_Columns.m_colNumber] = -1;
+	row[m_Columns.m_colText] = "Sum of all channels";
+	
+	int numberOfChannels = GlobalSettings::inputDevice->numberOfChannels();
+	for(int i=0; i<numberOfChannels; i++)
+	{
+		Gtk::TreeModel::iterator iter = m_refListStore->append();
+		Gtk::TreeModel::Row row = *iter;
+		row[m_Columns.m_colText] = std::to_string(i);
+		row[m_Columns.m_colNumber] = i;
+	}
+
+    m_dialChannelView.set_model(m_refListStore);
+
+    m_dialChannelView.append_column("Dial Channel", m_Columns.m_colText);
+	
 	
     add(m_mainLayoutGrid);
 	
@@ -31,6 +65,9 @@ SettingsPage::SettingsPage() :
 	m_mainLayoutGrid.show();
 	m_samplingIntervalScale.show();
 	m_samplingIntervalLabel.show();
+	m_dialChannelLabel.show();
+    m_dialChannelView.show();
+	m_ScrolledWindow.show();
 
 	signal_show().connect(sigc::mem_fun(*this, &SettingsPage::loadSettings));
 }
--- src/SettingsPage.hpp
+++ src/SettingsPage.hpp
@@ -18,6 +18,29 @@ private:
 	Gtk::Label m_samplingIntervalLabel;
 	Glib::RefPtr<Gtk::Adjustment> m_samplingIntervalAdjustment;
 	Gtk::Scale m_samplingIntervalScale;
+
+
+	Gtk::Label m_dialChannelLabel;
+	Gtk::ScrolledWindow m_ScrolledWindow;
+	Gtk::TreeView m_dialChannelView;
+	//Glib::RefPtr<Gtk::TreeModel> m_dialChannelModel;
+
+	class DialModelColumns : public Gtk::TreeModelColumnRecord
+	{
+	public:	
+	    DialModelColumns()
+		{
+			add(m_colText);
+			add(m_colNumber);
+		}
+		
+		Gtk::TreeModelColumn<Glib::ustring> m_colText;
+		Gtk::TreeModelColumn<int> m_colNumber;
+	};
+
+    DialModelColumns m_Columns;
+
+	Glib::RefPtr<Gtk::ListStore> m_refListStore;
 };
 
 #endif
