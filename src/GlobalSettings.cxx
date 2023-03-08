32b485806b05210f26e29a4364688b606116f6ca
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-08 15:21:58 +0000

    add 16 channel device into the global settings

--- src/GlobalSettings.cxx
+++ src/GlobalSettings.cxx
@@ -2,8 +2,11 @@
 #include "GlobalSettings.hpp"
 #include "MockDevice.hpp"
 #include "SingleChannelUartDevice.hpp"
+#include "SixteenChannelUartDevice.hpp"
 
 std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new MockDevice);
 //std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new SingleChannelUartDevice);
+//std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new SixteenChannelUartDevice);
+
 int GlobalSettings::samplingInterval_ms = 1000;
 int GlobalSettings::dialChannel = 0;
