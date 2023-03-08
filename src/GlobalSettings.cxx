2db72b8484f01f81d82a2902fa8b2a54e916ff7e
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-08 14:49:37 +0000

    set default device to the mock device

--- src/GlobalSettings.cxx
+++ src/GlobalSettings.cxx
@@ -3,5 +3,5 @@
 #include "MockDevice.hpp"
 #include "SingleChannelUartDevice.hpp"
 
-//std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new MockDevice);
-std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new SingleChannelUartDevice);
+std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new MockDevice);
+//std::shared_ptr<IInputDevice> GlobalSettings::inputDevice = std::shared_ptr<IInputDevice>(new SingleChannelUartDevice);
