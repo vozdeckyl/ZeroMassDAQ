4431bd19fe579f1c87707510a042ba2c109be424
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-13 13:49:17 +0000

    added a simple table with devices and radio buttons

--- src/DevicesPage.cxx
+++ src/DevicesPage.cxx
@@ -1,5 +1,5 @@
 #include "DevicesPage.hpp"
-// #include "GlobalSettings.hpp"
+#include "GlobalSettings.hpp"
 
 DevicesPage::DevicesPage()
 {
@@ -7,10 +7,38 @@ DevicesPage::DevicesPage()
     m_label.set_margin_bottom(20);
 
 	m_mainLayoutGrid.insert_row(0);
+	m_mainLayoutGrid.insert_row(0);
+	
+	m_deviceTable.insert_column(0);
+	m_deviceTable.insert_column(0);
+	
+	m_deviceTable.set_row_spacing(10);
+	m_deviceTable.set_column_spacing(10);
+
+	int index{0};
+	for(auto device : GlobalSettings::inputDevices)
+	{
+		m_deviceTable.insert_row(index);
+		
+		m_labels.push_back(std::shared_ptr<Gtk::Label>(new Gtk::Label(device->deviceName())));
+		m_labels.back()->show();
+		m_labels.back()->set_halign(Gtk::Align::ALIGN_START);
+		m_deviceTable.attach(*(m_labels.back()),1,index);
+		
+		m_radioButtons.push_back(std::shared_ptr<Gtk::RadioButton>(new Gtk::RadioButton));
+		m_radioButtons.back()->show();
+		m_radioButtons.back()->set_group(m_buttonGroup);
+		m_deviceTable.attach(*(m_radioButtons.back()),0,index);
+		index++;
+	}
+	
 	m_mainLayoutGrid.attach(m_label,0,0);
+	m_mainLayoutGrid.attach(m_deviceTable,0,1);
+	
 
 	add(m_mainLayoutGrid);
 	
+	m_deviceTable.show();
 	m_mainLayoutGrid.show();
 	m_label.show();
 }
--- src/DevicesPage.hpp
+++ src/DevicesPage.hpp
@@ -1,6 +1,8 @@
 #ifndef DEVICESPAGE_H
 #define DEVICESPAGE_H
 
+#include <vector>
+#include <memory>
 #include <gtkmm.h>
 
 class DevicesPage : public Gtk::Box {
@@ -10,6 +12,11 @@ public:
 private:
     Gtk::Label m_label;
     Gtk::Grid m_mainLayoutGrid;
+	Gtk::Grid m_deviceTable;
+	std::vector<std::shared_ptr<Gtk::Label>> m_labels;
+	std::vector<std::shared_ptr<Gtk::RadioButton>> m_radioButtons;
+	Gtk::RadioButtonGroup m_buttonGroup;
+	
 };
 
 #endif
