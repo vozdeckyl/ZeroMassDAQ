81f8ccca06abf9727a02b99b5d63261e0a940d4a
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-16 16:07:48 +0000

    icon for the exit button

--- src/MainWindow.cxx
+++ src/MainWindow.cxx
@@ -13,7 +13,7 @@ MainWindow::MainWindow()
         {std::shared_ptr<Gtk::Image>(new Gtk::Image("icons/measurement.png")), std::shared_ptr<MeasurementPage>(new MeasurementPage(32))},
         {std::shared_ptr<Gtk::Image>(new Gtk::Image("icons/settings.png")), std::shared_ptr<SettingsPage>(new SettingsPage())},
         {std::shared_ptr<Gtk::Image>(new Gtk::Image("icons/inputDevice.png")), std::shared_ptr<DevicesPage>(new DevicesPage)},
-        {std::shared_ptr<Gtk::Label>(new Gtk::Label("Exit")), std::shared_ptr<ExitPage>(new ExitPage(this))}
+        {std::shared_ptr<Gtk::Image>(new Gtk::Image("icons/exit.png")), std::shared_ptr<ExitPage>(new ExitPage(this))}
     };
         
     make_slides(slides, 100, 10);
