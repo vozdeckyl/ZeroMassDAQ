86f41867e8d38a6ef88d7cdeff41a549e39c09af
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-03-18 09:27:45 +0000

    ask confirming question in the uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-rm -r /usr/share/ZeroMassDAQ
-rm /usr/share/applications/zeromassdaq.desktop
+function uninstall()
+{
+    echo "Uninstalling Zero Mass DAQ"
+
+    echo "- rm -r /usr/share/ZeroMassDAQ"
+    rm -r /usr/share/ZeroMassDAQ
+
+    echo "- rm /usr/share/applications/zeromassdaq.desktop"
+    rm /usr/share/applications/zeromassdaq.desktop
+
+    echo "Done"
+}
+
+while true; do
+    read -p "Do you really want it uninstall Zero Mass DAQ? [Y/n]: " yn
+    case $yn in
+	[Y]*) uninstall; break  ;;
+	[Nn]*) echo "Aborted"; break ;;
+    esac
+done
+
+
